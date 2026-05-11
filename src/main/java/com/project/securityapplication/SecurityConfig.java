6ccd1775c7ae1843a012dbc7a17d330eda1cbbe7
omsolanki271 <omsolanki271@gmail.com>
2026-05-11 20:17:53 +0530

    Implement basic authentication with httpBasic

--- src/main/java/com/project/securityapplication/SecurityConfig.java
+++ src/main/java/com/project/securityapplication/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.project.securityapplication;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -11,8 +12,10 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http)
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
     {
-
+        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+        http.httpBasic(Customizer.withDefaults());
+        return http.build();
     }
 }
