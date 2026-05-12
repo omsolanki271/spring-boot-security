d329f337ab311cad18ca0b13c99b262dd528028d
omsolanki271 <omsolanki271@gmail.com>
2026-05-12 23:48:37 +0530

    Add InMemoryUserDetailsManager with UserDetails authentication setup

--- src/main/java/com/project/securityapplication/SecurityConfig.java
+++ src/main/java/com/project/securityapplication/SecurityConfig.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,5 +21,15 @@ public class SecurityConfig {
         http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
         http.httpBasic(Customizer.withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService()
+    {
+        UserDetails user1 = User.withUsername("user1")
+                .password("{noop}123")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1);
     }
 }
