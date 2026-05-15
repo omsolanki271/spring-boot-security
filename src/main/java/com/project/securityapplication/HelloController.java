5f0b5e3b043489045ce3b0464ce6c3f415090c4e
Solanki Om <omsolanki271@gmail.com>
2026-05-15 23:25:11 +0530

    Remove sayHello method from HelloController
    Removed the sayHello method from HelloController.

--- src/main/java/com/project/securityapplication/HelloController.java
+++ src/main/java/com/project/securityapplication/HelloController.java
@@ -5,11 +5,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @GetMapping("/hello")
-    public String sayHello()
-    {
-        return "Hello";
-    }
 
     @GetMapping("/")
     public String home()
