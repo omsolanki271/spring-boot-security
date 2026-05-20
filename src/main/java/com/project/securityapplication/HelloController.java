c96b5aee9bb7f8f3c01aa2f3891c41ef7d9305f6
omsolanki271 <omsolanki271@gmail.com>
2026-05-21 00:04:13 +0530

    Add @PreAuthorize role-based method security for test endpoint

--- src/main/java/com/project/securityapplication/HelloController.java
+++ src/main/java/com/project/securityapplication/HelloController.java
@@ -1,10 +1,19 @@
 package com.project.securityapplication;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
+    //@PreAuthorize("hasRole('admin')")
+    @PreAuthorize("hasAnyRole('admin','user')")
+    @GetMapping("test")
+    public String test()
+    {
+        return  "Test Working";
+    }
 
     @GetMapping("/")
     public String home()
