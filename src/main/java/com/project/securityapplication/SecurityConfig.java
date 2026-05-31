95ba90dce5abf5ce5f4ddb5bfddeb2aea40f6749
omsolanki271 <omsolanki271@gmail.com>
2026-05-31 23:49:36 +0530

    Remove unused InMemoryUserDetailsManager import

--- src/main/java/com/project/securityapplication/SecurityConfig.java
+++ src/main/java/com/project/securityapplication/SecurityConfig.java
@@ -9,7 +9,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -26,10 +25,9 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
     {
         http.authorizeHttpRequests(auth -> auth
-                .requestMatchers("/").permitAll()
+                .requestMatchers("/", "/about").permitAll()
                 .requestMatchers("/admin").hasRole("ADMIN")
                 .requestMatchers("/user").hasRole("USER")
-                .requestMatchers("/","/about").permitAll()
                 .anyRequest().authenticated());
         http.httpBasic(Customizer.withDefaults());
 
